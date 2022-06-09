db8546e460bd63fdc13f9d7c11bc62d0c4a2d82e
Pablot123 <pablotamayo44@gmail.com>
2022-06-08 20:13:51 -0500

    add comments punto_4.py

--- punto_4.py
+++ punto_4.py
@@ -1,10 +1,12 @@
 generation = int(input('Enter the generation: '))
 
 total_family_members = 0
-for i in range(generation+1):
-    generation_family_members = 2**i
-    total_family_members += 2**i
+#we add 1 to generation becaus we need to calculate until the current generation
+# and the range ends on the previus number
+for i in range(generation+1): 
+    generation_family_members = 2**i # the member on each generations
+    total_family_members += 2**i  # constantly add the number of each generation
     print(f'Generation {i} have {generation_family_members}')
     
 
-print(f'Total number of people in the family are: {total_family_members}')
+print(f'Total number of members in the family: {total_family_members}')
